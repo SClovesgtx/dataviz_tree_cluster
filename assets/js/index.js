e9be06cb30f4bfee5af37648c8347180d3d06b90
SClovesgtx <clovestudent@gmail.com>
2022-02-06 15:58:17 -0300

    tree list fo cadeia, temas and subtemas

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,7 +1,47 @@
-const fs = require('fs');
+// function nestListItems(ulElement, items, className) {
+//     ulElement.setAttribute("class", className)
+//     for 
+//     if (!items.?buckets){
+//         return ulElement
+//     }
+// }
 
-const readData = () => JSON.parse(fs.readFileSync(__dirname + "/data.json", "utf8"));
 
-const data = readData()
+fetch('./data.json')
+    .then(response => response.json())
+    .then(data => {
+        const rootElement = document.querySelector('#root')
+        const cadeias = data["cadeias"]["buckets"]
+        const firstUl = document.createElement("ul")
+        firstUl.setAttribute("class", "cadeia")
+        for (let cadeia of cadeias) {
+            let li = document.createElement("li")
+            li.setAttribute("id", cadeia["key"])
+            li.innerHTML = cadeia["key"]
+            firstUl.appendChild(li)
+            let temas = cadeia["temas"]["buckets"]
+            let ulTemas = document.createElement("ul")
+            ulTemas.setAttribute("class", "tema")
+            for (let tema of temas){
+                let liTema = document.createElement("li")
+                liTema.setAttribute("id", tema["key"])
+                liTema.innerHTML = tema["key"]
+                let subtemas = tema?.subtemas?.buckets
+                if (subtemas) {
+                    let ulSubtemas = document.createElement("ul")
+                    ulSubtemas.setAttribute("class", "subtemas")
+                    for (let subtema of subtemas) {
+                        let liSubtema = document.createElement("li")
+                        liSubtema.setAttribute("id", subtema["key"])
+                        liSubtema.innerHTML = subtema["key"]
+                        ulSubtemas.appendChild(liSubtema)
+                    }
+                    liTema.appendChild(ulSubtemas)
+                }
+                ulTemas.appendChild(liTema)
+            }
+            firstUl.appendChild(ulTemas)
+        };
 
-console.log(data["cadeias"])
+        rootElement.appendChild(firstUl)
+    });
