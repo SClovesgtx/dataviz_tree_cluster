ca8172649d29bf12cd3998aa1cd8b2cda6c470aa
SClovesgtx <clovestudent@gmail.com>
2022-02-07 12:36:06 -0300

    conectando nós

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,12 +1,5 @@
-// function nestdivstItems(divElement, items, className) {
-//     divElement.setAttribute("class", className)
-//     for 
-//     if (!items.?buckets){
-//         return divElement
-//     }
-// }
 
-
+// https://github.com/musclesoft/jquery-connections/wiki
 fetch('./data.json')
     .then(response => response.json())
     .then(data => {
@@ -14,26 +7,35 @@ fetch('./data.json')
         const cadeias = data["cadeias"]["buckets"]
         const firstDiv = document.createElement("div")
         firstDiv.setAttribute("id", "cadeias")
+        let cadeiaNumber = 0
         for (let cadeia of cadeias) {
             let divCadeia = document.createElement("div")
             divCadeia.setAttribute("id", cadeia["key"])
             divCadeia.setAttribute("class", "cadeia")
-            divCadeia.innerHTML = `<div class="cadeiaName">${cadeia["key"]}</div>`
+            let cadeiaName = cadeia['key'].replace(/[ \/]/gm, '-')
+            let cadeiaId = `cadeia-${cadeiaName}-${cadeiaNumber+=1}`
+            divCadeia.innerHTML = `<div id='${cadeiaId}' class="cadeiaName">${cadeia["key"]}</div>`
             let temas = cadeia["temas"]["buckets"]
             let divTemas = document.createElement("div")
             divTemas.setAttribute("class", "temas")
+            let temaNumber = 0
             for (let tema of temas){
                 let divTema = document.createElement("div")
                 divTema.setAttribute("id", tema["key"])
                 divTema.setAttribute("class", "tema")
-                divTema.innerHTML = `<div class="temaName">${tema["key"]}</div>`
+                let temaName = tema['key'].replace(/[ \/]/gm, '-')
+                let temaId = `${cadeiaName}-${temaName}-${temaNumber+=1}`
+                divTema.innerHTML = `<div id='${temaId}' class="temaName">${tema["key"]}</div>`
                 let subtemas = tema?.subtemas?.buckets
                 if (subtemas) {
                     let divSubtemas = document.createElement("div")
                     divSubtemas.setAttribute("class", "subtemas")
+                    let subTemaNumber = 0
                     for (let subtema of subtemas) {
+                        let subTemaName = subtema['key'].replace(/[ \/]/gm, '-')
+                        let subTemaId = `${cadeiaName}-${temaName}-${subTemaName}-${subTemaNumber+=1}`
                         let divSubtema = document.createElement("div")
-                        divSubtema.setAttribute("id", subtema["key"])
+                        divSubtema.setAttribute("id", subTemaId)
                         divSubtema.setAttribute("class", "subtema")
                         divSubtema.innerHTML = subtema["key"]
                         divSubtemas.appendChild(divSubtema)
@@ -46,4 +48,28 @@ fetch('./data.json')
         };
 
         rootElement.appendChild(firstDiv)
+        cadeiaNumber = 0
+        for (let cadeia of cadeias){
+            let temas = cadeia["temas"]["buckets"]
+            let cadeiaName = cadeia['key'].replace(/[ \/]/gm, '-')
+            let cadeiaId = `cadeia-${cadeiaName}-${cadeiaNumber+=1}`
+            temaNumber = 0
+            for (let tema of temas){
+                let temaName = tema['key'].replace(/[ \/]/gm, '-')
+                let temaId = `${cadeiaName}-${temaName}-${temaNumber+=1}`
+                let query = `#${cadeiaId}, #${temaId}`
+                $(query).connections();
+                let subtemas = tema?.subtemas?.buckets
+                if (subtemas) {
+                    subTemaNumber = 0
+                    for (let subtema of subtemas) {
+                        let subTemaName = subtema['key'].replace(/[ \/]/gm, '-')
+                        let subTemaId = `${cadeiaName}-${temaName}-${subTemaName}-${subTemaNumber+=1}`
+                        let query = `#${temaId}, #${subTemaId}`
+                        $(query).connections();
+                    }
+                }
+            }
+        }
+        // addConnections(data)
     });
